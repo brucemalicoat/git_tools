3b3fb2a40c872019b8942ffd46b11e0e5d523196
Bruce Malicoat <malicoat.bruce@gmail.com>
2026-01-26 09:31:12 -0600

    added Application class

--- PythonApps/001_git_tools/gittools.py
+++ PythonApps/001_git_tools/gittools.py
@@ -1,13 +1,10 @@
 project_directory = """c:/git/github/git_tools/PythonApps/001_git_tools"""
-#region imports
 import  os
 os.chdir(project_directory)
 import sys
-import tkinter
-import pandastable
 sys.path.append('../..')
 from    PythonClasses.Application     import  Application
-#endregion imports
+
 
 myApp = Application(    arg_title               =       "gittools v1.0.0")
 myApp.readConig(        arg_config_filename     =       "gittools.ini")
